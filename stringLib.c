3614405e054b4c254a70a041b5c9c2e3ead79ca8
user-0021 <nabeyamaoden@gmail.com>
2025-02-17 12:07:08 +0900

    add file

---
+++ stringLib.c
@@ -0,0 +1,36 @@
+#include "stringLib.h"
+
+int strlistOR(char const* str1,int argc,...){
+    va_list args;
+    va_start(args, argc);
+
+	int i;
+	for(i = 0;i < argc;i++){
+		if(strcmp(str1,va_arg(args,char*)) == 0){
+			va_end(args);
+			return 0;
+		}
+	}
+
+    va_end(args);
+
+	return 1;
+}
+
+char* strGetMatchPattern(char const* str1,int argc,...){
+	va_list args;
+    va_start(args, argc);
+
+	int i;
+	for(i = 0;i < argc;i++){
+		char* tmp = va_arg(args,char*);
+		if(strcmp(str1,tmp) == 0){
+			va_end(args);
+			return tmp;
+		}
+	}
+
+    va_end(args);
+
+	return NULL;
+}
---
+++ stringLib.h
@@ -0,0 +1,9 @@
+/**
+ * 文字列を扱うライブラリです。 
+ * 
+ * 複数の文字列の比較などのシンプルな関数を実装しています
+ */
+#pragma once
+
+int strlistOR(char const* str1,int argc,...);
+char* strGetMatchPattern(char const* str1,int argc,...);
